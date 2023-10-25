f98ce98f1ef78c0ad775cf9d0234e4b0ad9e0970
hakimova98 <148937681+hakimova98@users.noreply.github.com>
2023-10-25 13:08:51 +0500

    Create case1.py

---
+++ case1.py
@@ -0,0 +1,36 @@
+'''x=int(input())
+if x<6:
+    print("Vinni_pukh")
+else:
+    if  x<12:
+       print('Afsonaho')
+    else:
+        if x<18:
+            print('Shohnoma')
+        else:
+            if x<24:
+                print('Tojikon')
+            else:
+                print('Shahzoda va gado') '''
+'''x=int(input())
+if x<6:
+    print("Vinni_pukh")
+if  x>6 and x<12:
+    print('Afsonaho')
+if  x>12 and x<18:
+    print('Shohnoma')
+if x>18 and x<24:
+    print('Tojikon')
+if x>24:
+    print('Shahzoda va gado')'''
+#x=int(input())
+if x<6:
+    print("Vinni_pukh")
+elif  x<12:
+    print('Afsonaho')
+elif x<18:
+    print('Shohnoma')
+elif x<24:
+    print('Tojikon')
+else:
+    print('Shahzoda va gado')
